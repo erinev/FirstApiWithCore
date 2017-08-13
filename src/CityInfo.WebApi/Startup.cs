79a7a5873bb5002439c4bf007d0bb7fc6a01edcd
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-08-13 22:47:26 +0300

    added swagger

--- src/CityInfo.WebApi/Startup.cs
+++ src/CityInfo.WebApi/Startup.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace CityInfo.WebApi
 {
@@ -12,6 +17,8 @@ namespace CityInfo.WebApi
     /// </summary>
     public class Startup
     {
+        private static IConfigurationRoot Configuration { get; set; }
+
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
         /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -19,9 +26,13 @@ namespace CityInfo.WebApi
         /// <param name="services">By default injected services list</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureApplicationConfigReader();
+
             services.AddMvc()
                 .AddJsonOptions(ConfigureNamingStrategy)
                 .AddMvcOptions(ConfigureOutputFormatters);
+
+            services.AddSwaggerGen(ConfigureSwaggerGen);
         }
 
         /// <summary>
@@ -41,9 +52,48 @@ namespace CityInfo.WebApi
             }
 
             app.UseMvc();
+
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swagger, httpRequest) => swagger.Host = httpRequest.Host.Value);
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
         }
 
         #region Private Functions
+
+        private void ConfigureApplicationConfigReader()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        private void ConfigureSwaggerGen(SwaggerGenOptions options)
+        {
+            string pathToDoc = Configuration["swagger:xmlDocsFileName"];
+
+            options.SwaggerDoc("v1",
+                new Info
+                {
+                    Title = "City Info API",
+                    Version = "v1",
+                    Description = "A simple API which allows to query and modify cities information.",
+                    TermsOfService = "None"
+                }
+            );
+
+            string fullXmlDocsFilePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, pathToDoc);
+            options.IncludeXmlComments(fullXmlDocsFilePath);
+
+            options.DescribeAllEnumsAsStrings();
+        }
 
         private void ConfigureOutputFormatters(MvcOptions options)
         {
