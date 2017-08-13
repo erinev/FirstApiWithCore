764a9f7f196af798467bbc666022eca2a80bde34
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-08-13 17:06:22 +0300

    added output formatters & naming strategy configuration

--- src/CityInfo.WebApi/Startup.cs
+++ src/CityInfo.WebApi/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Buffers;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.WebApi
@@ -11,7 +16,9 @@ namespace CityInfo.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureMvc(services);
+            services.AddMvc()
+                .AddJsonOptions(ConfigureNamingStrategy)
+                .AddMvcOptions(ConfigureOutputFormatters);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,18 +38,27 @@ namespace CityInfo.WebApi
 
         #region Private Functions
 
-        private void ConfigureMvc(IServiceCollection services)
+        private void ConfigureOutputFormatters(MvcOptions options)
         {
-            services.AddMvc()
-                .AddJsonOptions(options =>
-                {
-                    var contractResolver = options.SerializerSettings?.ContractResolver as DefaultContractResolver;
+            options.OutputFormatters.Clear();
 
-                    if (contractResolver != null)
-                    {
-                        contractResolver.NamingStrategy = new CamelCaseNamingStrategy();
-                    }
-                });
+            options.OutputFormatters.Add(
+                new JsonOutputFormatter(new JsonSerializerSettings(), ArrayPool<char>.Shared)
+            );
+
+            options.OutputFormatters.Add(
+                new XmlDataContractSerializerOutputFormatter()
+            );
+        }
+
+        private void ConfigureNamingStrategy(MvcJsonOptions options)
+        {
+            var contractResolver = options.SerializerSettings?.ContractResolver as DefaultContractResolver;
+
+            if (contractResolver != null)
+            {
+                contractResolver.NamingStrategy = new CamelCaseNamingStrategy();
+            }
         }
 
         #endregion
