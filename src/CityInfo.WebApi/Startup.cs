db2e9f004d9e1ed2501b3d96abeaa11b7c1e4333
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-08-13 13:18:17 +0300

    added MVC middle ware

--- src/CityInfo.WebApi/Startup.cs
+++ src/CityInfo.WebApi/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.WebApi
@@ -11,6 +10,7 @@ namespace CityInfo.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +25,12 @@ namespace CityInfo.WebApi
                 app.UseExceptionHandler();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc();
+
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
         }
     }
 }
