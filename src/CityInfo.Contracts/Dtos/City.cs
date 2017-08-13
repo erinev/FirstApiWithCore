73bf23eae6247768bb9da27123c34534c321a896
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-08-13 14:16:53 +0300

    introduced contracts

---
+++ src/CityInfo.Contracts/Dtos/City.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.Contracts.Dtos
+{
+    public class City
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ src/CityInfo.WebApi/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CityInfo.Contracts.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.WebApi.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+        public ActionResult GetCities()
+        {
+            var cities = new List<City>
+            {
+                new City { Id = 1, Name = "Birštonas", Description = "City of dreams" },
+                new City { Id = 2, Name = "Kaunas", Description = "City of contruction" },
+                new City { Id = 3, Name = "Vilnius", Description = "City of wasted money" }
+
+            };
+
+            return Ok(cities);
+        }
+    }
+}
