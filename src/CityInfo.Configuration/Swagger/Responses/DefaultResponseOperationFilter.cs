74134bea1d48dd1d4f71b802e8b495998bbc29c9
Erikas Neverdauskas <e.neverdauskas@adform.com>
2017-08-17 08:53:59 +0300

    added 401 response message to all endpoints operation responses

--- src/CityInfo.Configuration/Swagger/Responses/DefaultResponseOperationFilter.cs
+++ src/CityInfo.Configuration/Swagger/Responses/DefaultResponseOperationFilter.cs
@@ -11,6 +11,12 @@ namespace CityInfo.Configuration.Swagger.Responses
     {
         public void Apply(Operation operation, OperationFilterContext context)
         {
+            operation.Responses.Add("401", new Response
+            {
+                Description = "unauthorized - Authorization has been denied for this request. For example, request might be missing an api key header.",
+                Schema = context.SchemaRegistry.GetOrRegister(typeof(UnauthorizedResponse))
+            });
+
             operation.Responses.Add("500", new Response
             {
                 Description = "server_error - The server encountered an unexpected condition that prevented it from fulfilling the request.",
